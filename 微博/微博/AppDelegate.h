5df9138f3fc643543a30e5a43a49c6c2ec858a4f
肖峰 <xiaofeng@gaiay.cn>
2018-04-27 07:35:45 +0800

    初始化微博项目

---
+++ 微博/微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微博
+//
+//  Created by 肖峰 on 2018/4/27.
+//  Copyright © 2018年 xiaofeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微博/微博/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  微博
+//
+//  Created by 肖峰 on 2018/4/27.
+//  Copyright © 2018年 xiaofeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
